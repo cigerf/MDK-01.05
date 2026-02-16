7ccfcfdd9b749ee33f0d502168c9ddd9acd33db0
st403 <st403@WS403-11>
2026-02-16 08:54:02 +0300

    Создал модели классов

---
+++ cafe/Models/Clients.cs
@@ -0,0 +1,11 @@
+﻿namespace cafe.Models
+{
+    public class Clients
+    {
+        public string NickName {  get; set; }
+        public string Fullname {  get; set; }
+        public int BonusPoint { get; set; }
+        public string Email {  get; set; }
+
+    }
+}
---
+++ cafe/Models/Orders.cs
@@ -0,0 +1,10 @@
+﻿namespace cafe.Models
+{
+    public class Order
+    {
+        public int NumberOrder {  get; set; }
+        public string OrderContains { get; set; }
+        public double TotalAmount {  get; set; }
+        public string Status {  get; set; }
+    }
+}
