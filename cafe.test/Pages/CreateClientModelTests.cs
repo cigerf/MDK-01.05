b2b82c1c4b224a0aac44230710702bf23e1eb33f
idekferewf <skadeshopp@gmail.com>
2026-05-18 09:27:43 +0300

    Сделал тест

---
+++ cafe.test/Pages/CreateClientModelTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using cafe.Data;
+using cafe.Models;
+using Xunit;
+using System.Linq;
+
+namespace cafe.test.Tests.Pages
+{
+    public class CreateClientModelTests
+    {
+        private ApplicationDbContext GetDbContext()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            return new ApplicationDbContext(options);
+        }
+
+        [Fact]
+        public void OnPost_ShouldReturnPage_WhenModelStateIsInvalid()
+        {
+            // Arrange
+            var context = GetDbContext();
+            var pageModel = new cafe.Pages.Clients.CreateModel(context);
+
+            pageModel.ModelState.AddModelError("NickName", "Required");
+
+            // Act
+            var result = pageModel.OnPost();
+
+            // Assert
+            Assert.IsType<PageResult>(result);
+            Assert.Equal(0, context.Clients.Count());
+        }
+    }
+}
